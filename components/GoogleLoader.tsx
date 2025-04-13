4a6c5d0e7996d5ee69554c3d3c3f80126eaa0a46
surajarya130 <surajarya130@gmail.com>
2025-04-13 21:40:16 +0530

    WIP - Starting integrating camera plugin

--- components/GoogleLoader.tsx
+++ components/GoogleLoader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GoogleLoader = () => {
   return (
-    <div className="google-dots">
+    <div id="google-dots">
       <div className="dot blue"></div>
       <div className="dot red"></div>
       <div className="dot yellow"></div>
